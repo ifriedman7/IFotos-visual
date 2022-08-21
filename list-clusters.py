88d39fc52ab37a8632605343a7733dfe2ab4eb62
ifriedman7 <97619375+ifriedman7@users.noreply.github.com>
2022-08-21 19:27:50 +0300

    Update list-clusters.py

--- list-clusters.py
+++ list-clusters.py
@@ -8,11 +8,11 @@ from boto3.s3.transfer import S3UploadFailedError
 from botocore.exceptions import ClientError
 
 
-    s3 = boto3.client('s3')
+s3 = boto3.client('s3')
 
-    try:
-        response = s3.list_clusters()
-        print(f"List is \n{response}\n")
-    except ClientError as err:
-        print(f"Couldn't get the list.")
-        print(f"\t{err.response['Error']['Code']}:{err.response['Error']['Message']}")
+try:
+    response = s3.list_clusters()
+    print(f"List is \n{response}\n")
+except ClientError as err:
+    print(f"Couldn't get the list.")
+    print(f"\t{err.response['Error']['Code']}:{err.response['Error']['Message']}")
