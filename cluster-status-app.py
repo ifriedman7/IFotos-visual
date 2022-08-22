9ca8d69da0c7d526878648a12a59fe26594790af
ifriedman7 <97619375+ifriedman7@users.noreply.github.com>
2022-08-22 18:18:52 +0300

    cluster status

---
+++ cluster-status-app.py
@@ -0,0 +1,48 @@
+from flask import Flask, request, render_template
+
+"""    With Flask as webapp
+"""
+
+import datetime
+import time
+import urllib.parse
+import urllib.request
+import urllib.error
+import json
+import io
+import os
+import sys
+import re
+
+import boto3
+from botocore.exceptions import ClientError
+import requests
+from requests_aws4auth import AWS4Auth
+#need pip install requests-aws4auth
+# Initialize Flask app with the template folder address
+app = Flask(__name__, template_folder='templates')
+
+@app.route('/')
+def cluster_status():
+    region = 'us-east-1' # e.g. us-west-1
+    service = 'eks'
+    credentials = boto3.Session().get_credentials()
+    awsauth = AWS4Auth(credentials.access_key, credentials.secret_key, region, service, session_token=credentials.token)
+    eks = boto3.client('eks')
+
+    try:
+        response = eks.list_clusters()
+        cluster_list = response['clusters']
+        print(f"List is \n{cluster_list}\n")
+    except ClientError as err:
+        print(f"Couldn't get the list.")
+        print(f"\t{err.response['Error']['Code']}:{err.response['Error']['Message']}")
+    if 'ike-eks-cluster' in cluster_list:
+        print(f"Cluster is up.")
+        cluster_up = True
+    else:
+        print(f"Cluster is down.")
+        cluster_up = False
+    return render_template('index.html', cluster_up)
+if __name__ == '__main__':
+    app.run(debug=True)
